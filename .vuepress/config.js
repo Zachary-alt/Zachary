924dea47cd54116ac11275a2e5eb03f5ac1cc247
zhengzheng_zhao <113074607@qq.com>
2021-08-02 20:13:02 +0800

    desc

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     base:'/zachary/',
     title: 'Zachary',
-    description: '前端技能集锦',
+    description: '前端学习笔记',
     themeConfig: {
         lastUpdated: 'Last Updated',
         logo: '/assets/img/logo.jpg',
@@ -24,67 +24,6 @@ module.exports = {
             {text: '前端算法和数据结构', link: '/algorithm/'},
             { text: 'github', link: 'https://github.com/Zachary-alt' },
         ],
-        // sidebar:[
-        //     {
-        //         title: 'Git',   // 必要的
-        //         path: '/git/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        //         children: [
-        //           ['/git/commitizen','git-commit规范']
-        //         ]
-        //     }, 
-        //     {
-        //         title: 'Vue',   // 必要的
-        //         path: '/vue/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        //         children: [
-        //           ['/vue/base','vue干货'],
-        //           ['/vue/source','源码解析'],
-        //           ['/vue/other','补充']
-        //         ]
-        //     },
-        //     {
-        //         title: 'React',   // 必要的
-        //         path: '/react/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        //         children: [
-        //           ['/react/base','react干货'],
-        //           ['/react/component','组件化'],
-        //           ['/react/redux','redux'],
-        //           ['/react/router','router'],
-        //           ['/react/umi','umi']
-        //         ]
-        //     },
-        //     {
-        //         title: 'ReactNative',   // 必要的
-        //         path: '/reactNative/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        //     },
-        //     {
-        //         title: '可视化',   // 必要的
-        //         path: '/visualization/',
-        //         children: [
-        //             ['/visualization/canvas','canvas'],
-        //             ['/visualization/konva','konva'],
-        //           ]
-        //     },
-        //     {
-        //         title: '微前端',
-        //         path: '/microFrontend/'
-        //     },
-        //     {
-        //         title: 'Node',   // 必要的
-        //         path: '/node/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        //         children: [
-        //           ['/node/http','网络编程'],
-        //           ['/node/mySql','mySql'],
-        //           ['/node/mongodb','mongodb'],
-        //         ]
-        //     },
-        //     {
-        //         title: 'Docker',   // 必要的
-        //         path: '/docker/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        //         children: [
-        //           ['/docker/mysql','搭建mysql服务'],
-        //         ]
-        //     },
-        // ],
         sidebar:'auto',
         displayAllHeaders: true
     }
